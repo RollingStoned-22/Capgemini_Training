662498c7965f5113f6101e08343a76e7956f607d
Shuvam <shuvammajumder21@gmail.com>
2025-03-20 10:35:06 +0530

    start

--- Initialise_2DArray.java
+++ Initialise_2DArray.java
@@ -10,7 +10,12 @@ public class Initialise_2DArray {
 		int m = sc.nextInt();
 		int n = sc.nextInt();
 		int[][] arr = new int[m][n];
+		System.out.println("Enter values: ");
 		for(int i = 0;  i < m; i++) {
+			for(int j = 0; j < n; j++) {
+				arr[i][j] =  sc.nextInt();			}
+		}
+		for(int i = 0; i < n; i++) {
 			System.out.println(Arrays.toString(arr[i]));
 		}
 	}
