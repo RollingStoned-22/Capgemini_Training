b26f5ff823f967759d0139579777a4a2f72b7bbc
Shuvam <shuvammajumder21@gmail.com>
2025-03-20 10:28:09 +0530

    start

---
+++ Initialise_2DArray.java
@@ -0,0 +1,17 @@
+package day8;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Initialise_2DArray {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter row and column length: ");
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		int[][] arr = new int[m][n];
+		for(int i = 0;  i < m; i++) {
+			System.out.println(Arrays.toString(arr[i]));
+		}
+	}
+}
