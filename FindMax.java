8eada627de7919bb51fbbcfb4e52ead4a76eb2fd
Shuvam <shuvammajumder21@gmail.com>
2025-03-20 13:16:35 +0530

    recess

--- FindMax.java
+++ FindMax.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 public class FindMax {
 	Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		int[][] arr = Initialise_2DArray.createArray();
-		int max_element = findMax(arr);
-		System.out.println("Maximum element: "+max_element);
+		int maxElement = findMax(arr);
+		System.out.println(maxElement);
 	}
+
 	public static int findMax(int[][] arr) {
 		int maxElement = arr[0][0];
-		for(int row[] :  arr) {
-			for(int num :  row) {
-				maxElement = (num > maxElement)?num:maxElement;
+		for (int row[] : arr) {
+			for (int num : row) {
+				maxElement = (num > maxElement) ? num : maxElement;
 			}
 		}
 		return maxElement;
