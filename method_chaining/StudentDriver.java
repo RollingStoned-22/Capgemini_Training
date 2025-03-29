9208703889865acc0fad6aab36a16eb357f3ef1b
RollingStoned-22 <132134205+RollingStoned-22@users.noreply.github.com>
2025-03-29 14:49:26 +0530

    Add files via upload

---
+++ method_chaining/StudentDriver.java
@@ -0,0 +1,10 @@
+package method_chaining;
+
+public class StudentDriver {
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.setName("Shuvam");
+		System.out.println(s);
+	}
+}
+
