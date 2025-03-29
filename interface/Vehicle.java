6bcd9107d5860e32dc220a4f07be80433a7fea8f
RollingStoned-22 <132134205+RollingStoned-22@users.noreply.github.com>
2025-03-29 14:28:50 +0530

    Create Vehicle.java

---
+++ interface/Vehicle.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+public interface Vehicle {
+	void start();
+
+	void stop();
+
+	void accelerate(int increment);
+
+	void brake(int decrement);
+
+	void getCurrentSpeed();
+
+	void displayType();
+}
