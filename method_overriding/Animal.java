a91066d517d14b664e99b490441f4fff2b07d277
RollingStoned-22 <132134205+RollingStoned-22@users.noreply.github.com>
2025-03-29 14:51:02 +0530

    Add files via upload

---
+++ method_overriding/Animal.java
@@ -0,0 +1,7 @@
+package method_overriding;
+
+public class Animal {
+	public void sound() {
+		System.out.println("Animal is making sound");
+	}
+}
---
+++ method_overriding/Dog.java
@@ -0,0 +1,11 @@
+package method_overriding;
+
+public class Dog extends Animal {
+	public String breed;
+	public Dog(String breed) {
+		this.breed = breed;
+	}
+	public void sound() {
+		System.out.println("Dog is barking");
+	}
+}
