60e0b6c56683ff978ba2621fdca4b15e8bf36d82
RollingStoned-22 <132134205+RollingStoned-22@users.noreply.github.com>
2025-03-29 14:42:40 +0530

    Add files via upload

---
+++ composition/Driver.java
@@ -0,0 +1,8 @@
+package composition;
+
+public class Driver {
+	public static void main(String[] args) {
+		Car C = new Car("Ferrari","V8");
+		C.display();
+	}
+}
---
+++ composition/MobileDriver.java
@@ -0,0 +1,8 @@
+package composition;
+
+public class MobileDriver {
+	public static void main(String[] args) {
+		Phone p = new Phone("Motorola Moto G84", "Li-ion");
+		p.display();
+	}
+}
