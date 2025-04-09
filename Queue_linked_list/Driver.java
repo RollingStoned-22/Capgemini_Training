e1609b3de4357108a8697746f143e0eee0649a74
RollingStoned-22 <132134205+RollingStoned-22@users.noreply.github.com>
2025-04-09 15:54:01 +0530

    Create Driver.java

---
+++ Queue_linked_list/Driver.java
@@ -0,0 +1,47 @@
+package queue_linked_list;
+
+import java.util.Scanner;
+
+
+public class Driver {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Queue q = new Queue();
+		boolean flag = true;
+		while(flag) {
+			System.out.println("1. Enqueue\n2. Dequeue\n3. Display Queue\n4. Is Queue Empty?\n5. Exit");
+			int ch = sc.nextInt();
+			switch(ch) {
+			case 1:
+				System.out.println("Enter data: ");
+				int data1 = sc.nextInt();
+				q.enqueue(data1);
+				break;
+			case 2:
+				int front_element = q.dequeue();
+				if(front_element == Integer.MIN_VALUE)
+					System.out.println("Queue is empty");
+				else
+					System.out.println(front_element);
+				break;
+			case 3:
+				q.display();
+				break;
+			case 4:
+				System.out.println(q.isEmpty());
+				break;
+			case 5:
+				flag = false;
+				System.out.println("Thank you");
+				break;
+			case 6:
+				int peek_element = q.peek();
+				if(peek_element == Integer.MIN_VALUE)
+					System.out.println("Queue is empty");
+				else
+					System.out.println(peek_element);
+			}
+		}
+	}
+}
+
