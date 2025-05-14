2e6059aa74c1cd4fe94f4982ac294146c1f5d39f
mavuhS-Tech <132134205+mavuhS-Tech@users.noreply.github.com>
2025-05-14 13:18:45 +0530

    Update PhNoValidation.java

--- regex/PhNoValidation.java
+++ regex/PhNoValidation.java
@@ -1,5 +1,15 @@
 package regex;
-
-public class PhNoValidation {
-	
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class PhoneNumberValidation {
+	public static void main(String[] args) {
+		String regex="[789]\\d{9}";
+		String number="9905881554,7605881554";
+		Pattern pattern=Pattern.compile(regex);
+		Matcher matcher=pattern.matcher(number);
+		while(matcher.find()) {
+			System.out.println(matcher.group()+" "+matcher.start());
+			}
+		
+	}
 }
